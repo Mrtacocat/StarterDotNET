8bf4b048c3386c5ea9428ba08c89ae0ad69b141e
Christoffer Riis <33607859+Mrtacocat@users.noreply.github.com>
2023-05-20 17:11:42 +0000

    Solve problem 2

---
+++ Problem2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorldCodeSpaces
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int sum = 0;
+            int limit = 4000000;
+            int previous = 1;
+            int current = 2;
+
+
+            while (current <= limit) {
+
+                if (current % 2 == 0) {
+                    sum += current;
+                }
+
+                int next = previous + current;
+                previous = current;
+                current = next;
+            }
+
+            Console.WriteLine("Sum: " + sum);
+
+
+        }
+    }
+}
